3444ada1a533f7a305fd63b7448bcd6558b6389b
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-03-20 13:11:52 +0500

    MERNLab 20/03/2024

---
+++ Paractice/Express.js/express-app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+let server= express();
+
+// Set view engine
+server.set("view engine", "ejs");
+
+// Serve static files from "public" directory
+server.use(express.static("public"));
+
+server.get("/api/stories",function(req,res){
+    res.send([
+        {title:"Story 1",Content:"story 1 content"},
+        {title:"Story 2",Content:"story 2 content"}
+    ])
+    });
+    server.get("/homepage",function(req,res){
+        res.render("homepage")
+        });
+server.get("/",function(req,res){
+res.send("Hello A Section")
+});
+
+server.listen(4000);  

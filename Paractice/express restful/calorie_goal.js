b5e76d612c4981c1cd6cc9534a5cdd866c381b9c
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-03-25 19:59:44 +0500

    calorie

--- Paractice/express restful/calorie_goal.js
+++ Paractice/express restful/calorie_goal.js
@@ -1,8 +1,17 @@
-const calorie_app= require("express")();
+const express= require("express");
+const calorie_app=express();
+calorie_app.use("json");
+
 function calculate_calorie(weight,height){
     return 100;
 }
 calorie_app.listen(4000,()=>console.log(`it's alive on`));
+
 calorie_app.get("/getcalorie",function(reques,recieve){
     recieve.send("hy");
+});
+calorie_app.post("/post_calorie",(req,res)=>{
+const {id}=req.params;
+const {logo}=req.body;
+logo?res.send({tshirt:`"with your logo ${logo} and id : ${id}"`}):res.status(418).send({message:'try logo'})
 })

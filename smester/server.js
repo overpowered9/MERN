8660cf7b799a9712929f9cd55f5db44c73fe620c
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-05-18 10:25:34 +0500

    Changes in Homepage

--- smester/server.js
+++ smester/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
-app.use(express.static('public'));
+app.use(express.static('./public/'));
 const port = 3000;
 
 app.set("view engine","ejs");

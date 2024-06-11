65a32b5164137b265805a9deeed85bf7356f3d12
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-06-11 22:18:01 +0500

    commit
    portfolio

--- Practice2/React/my-portfolio/src/components/animated_/animatedimage.jsx
+++ Practice2/React/my-portfolio/src/components/animated_/animatedimage.jsx
@@ -3,8 +3,8 @@ import 'animate.css/animate.min.css';
 import dpi from '../../assets/image.png'
 const AnimatedImage = () => {
     return (
-        <div className="w-[40%] aspect-auto left-0 sm:left-[10vw] relative sm:w-[50%]  overflow-hidden">
-            <img alt='pic' src={dpi} className="animate__animated animate__fadeInUp animate__slow sm:w-[300px] "/>
+        <div className="flex flex-shrink-0 aspect-auto left-0 sm:left-[10vw] sm:flex-none relative sm:w-[50%]  overflow-hidden">
+            <img alt='pic' src={dpi} className="bottom-0 animate__animated animate__fadeInUp animate__slow sm:w-[300px] "/>
         </div>
     );
 };

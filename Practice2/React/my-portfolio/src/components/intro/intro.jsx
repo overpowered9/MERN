62175f0ed9f2efa525e93190ba36d28a2d6a2a07
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-06-11 22:18:04 +0500

    Update intro.jsx

--- Practice2/React/my-portfolio/src/components/intro/intro.jsx
+++ Practice2/React/my-portfolio/src/components/intro/intro.jsx
@@ -3,9 +3,9 @@ import { Typewriter } from 'react-simple-typewriter'
 
 const Intro = () => {
     return (
-        <div className="w-[60vw] p-[5vw] sm:mt-12 overflow-hidden"> 
-        <p className="font-mono text-sm sm:text-xl font-extrabold">Hi, I am</p>
-            <div className=" text-lg text-[#5ea5b6] font-bold font-mono sm:text-3xl">
+        <div className=" w-[100vw] p-[5vw] sm:mt-12 sm:w-[60vw] overflow-hidden"> 
+        <p className="font-mono text-2xl sm:text-xl font-extrabold">Hi, I am</p>
+            <div className=" text-2xl text-[#5ea5b6] font-bold font-mono sm:text-3xl">
                 
             <Typewriter cursor typeSpeed={100} 
             cursorStyle='.' cursorBlinking={false} words={['Zaid Asghar Virk']}></Typewriter> 

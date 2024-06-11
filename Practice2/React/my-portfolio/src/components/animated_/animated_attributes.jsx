e821bff92ed253dc2945a64184ee7571a9eefa59
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-06-11 22:18:15 +0500

    Update animated_attributes.jsx

--- Practice2/React/my-portfolio/src/components/animated_/animated_attributes.jsx
+++ Practice2/React/my-portfolio/src/components/animated_/animated_attributes.jsx
@@ -6,7 +6,7 @@ import solution from '../../assets/solution.png';
 import equal from '../../assets/equal.png';
 const AnimatedAttr = () => {
     return (
-        <div className=" sm: gap-4 px-[100px] py-7  justify-between overflow-hidden flex flex-row flex-wrap animate__animated animate__fadeInUp animate__slow">
+        <div className=" sm: gap-4 px-[50px] py-7  justify-between overflow-hidden flex flex-row flex-wrap animate__animated animate__fadeInUp animate__slow">
             <Card imageSrc={adapt} text='Adaptability'></Card>
             <Card imageSrc={solution} text='Problem solving' ></Card>
             <Card imageSrc={equal} text='Attention to detail'></Card>
